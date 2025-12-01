36efbe90365f85ecd00f870caa6a4f3425f0a8d3
Tristan Carel <tcarel@umass.edu>
2025-12-01 00:59:25 -0500

    day 1 updated

--- Advent of Code 2025/obj/Debug/net9.0/Advent of Code 2025.AssemblyInfo.cs
+++ Advent of Code 2025/obj/Debug/net9.0/Advent of Code 2025.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Advent of Code 2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec1885c70251e13a3dccda7ae43b97c12796e46b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Advent of Code 2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Advent of Code 2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
